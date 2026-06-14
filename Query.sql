e0a5ce254c29638d638a421017bba231a03d24bb
Apurba Goon <apurbagoon1@gmail.com>
2026-06-14 16:19:22 +0600

    added query 6, 7

--- Query.sql
+++ Query.sql
@@ -158,3 +158,35 @@ from
   left join bookings as b on u.user_id = b.user_id
 order by
   u.user_id;
+
+
+-- Query 6 --
+select
+  booking_id,
+  match_id,
+  total_cost
+from
+  bookings
+where
+  total_cost > (
+    select
+      avg(total_cost)
+    from
+      bookings
+  );
+
+
+-- Query 7 --
+
+select
+  match_id,
+  fixture,
+  base_ticket_price
+from
+  matches
+order by
+  base_ticket_price desc
+limit
+  2
+offset
+  1;
